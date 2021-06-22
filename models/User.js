141e0f152f23a897d0ca8a013d82814b299128b7
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-22 14:40:14 +0200

    Creating User and Post models

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const { model, schema } = require('mongoose')
+
+const userSchema = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    createdAt: Date
+})
+
+module.exports = model('User', userSchema)
