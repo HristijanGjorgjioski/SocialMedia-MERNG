9a4e3f1e824750ff8bfcdbe4492200c79ffa0979
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-23 13:01:08 +0200

    Login and Register is done

--- util/validators.js
+++ util/validators.js
@@ -14,3 +14,16 @@ module.exports.validateRegisterInput = (username, email, password, confirmPasswo
         valid: Object.keys(errors).length < 1
     }
 }
+
+module.exports.validateLoginInput = (username, password) => {
+    const errors = {};
+
+    (username.trim === '') ? errors.username = 'Username must not be empty' : null;
+
+    (password.trim === '') ? errors.password = 'Password must not be empty' : null;
+
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
