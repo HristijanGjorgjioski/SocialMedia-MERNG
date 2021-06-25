8bb19e1497742919572edada500f548629575e3c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-25 17:30:30 +0200

    Working on Delete post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -5,7 +5,7 @@ module.exports = {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find()
+                const posts = await Post.find().sort({ createdAt: -1 })
                 return posts
             } catch(err) {
                 throw new Error(err)
@@ -34,6 +34,18 @@ module.exports = {
             const post = await newPost.save()
 
             return post
+        },
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context)
+
+            try {
+                const post = await Post.findById(postId)
+                if(user.username === ) {
+                    
+                }
+            } catch (error) {
+                
+            }
         }
     }
 }
