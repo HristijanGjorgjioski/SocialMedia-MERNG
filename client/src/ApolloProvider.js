37d53568b0597c485c64e1cd15a1b92f6be2cfab
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-06-30 16:07:22 +0200

    Creating ApolloServer

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+import App from './App'
+
+const httpLink = createHttpLink({
+    uri: "http://localhost:5000"
+})
+
+
