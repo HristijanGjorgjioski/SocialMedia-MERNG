cc3de36fafd34506a9443d529d2c7715702dd22d
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-07-12 15:00:25 +0200

    Facing some weird bug

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ApolloClient from 'apollo-client'
+import ApolloClient from '@apollo/client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createHttpLink } from 'apollo-link-http'
 import { ApolloProvider } from '@apollo/react-hooks'
