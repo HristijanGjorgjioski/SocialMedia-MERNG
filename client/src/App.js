f4978373640aad396ef2d1b2af6d5d1af93620eb
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-07-26 17:40:20 +0200

    Importing routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import 'semantic-ui-css/semantic.min.css'
 
-
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
 
 const App = () => {
     return (
