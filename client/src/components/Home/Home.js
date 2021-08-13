30946a3ce81cf880c7d30f6545de7ac977dc814d
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-13 20:02:19 +0200

    Working on Home page and creating a gql query

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import gql from 'graphql'
 
 const Home = () => {
     return (
@@ -7,5 +8,18 @@ const Home = () => {
         </div>
     )
 }
+
+const FETCH_POSTS_QUERY = gql`
+    getPosts{
+        id body createdAt likeCount
+        likes{
+            username
+        }
+        commentCount
+        comments{
+            id username createdAt body
+        }
+    }
+`
 
 export default Home
