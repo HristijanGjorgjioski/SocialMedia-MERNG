cfe030bf3f825ed6d5385a15f4e8c482529f011d
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-14 18:35:45 +0200

    Working on PostCard interface

--- client/src/components/PostCard/PostCard.js
+++ client/src/components/PostCard/PostCard.js
@@ -1,31 +1,19 @@
 import React from 'react'
 
 import { Card, Icon, Label } from 'semantic-ui-react'
+import moment from 'moment'
 
 const PostCard = ( { post: { body, createdAt, id, username, likeCount, commentCount, likes} } ) => {
     return (
         <Card>
             <Card.Content>
-                <Image
-                floated='right'
-                size='mini'
-                src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
-                />
-                <Card.Header>Steve Sanders</Card.Header>
-                <Card.Meta>Friends of Elliot</Card.Meta>
-                <Card.Description>
-                Steve wants to add you to the group <strong>best friends</strong>
-                </Card.Description>
+                <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/molly.jpg' />
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <div className='ui two buttons'>
-                    <Button basic color='green'>
-                        Approve
-                    </Button>
-                    <Button basic color='red'>
-                        Decline
-                    </Button>
-                </div>
+                <p>buttons here</p>
             </Card.Content>
         </Card>
     )
