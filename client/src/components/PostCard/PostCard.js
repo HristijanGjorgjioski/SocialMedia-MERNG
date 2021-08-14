f12120bab7b8735c40c20cb9f298a65000db6243
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-14 18:42:47 +0200

    Working on Like and Comment buttons

--- client/src/components/PostCard/PostCard.js
+++ client/src/components/PostCard/PostCard.js
@@ -1,19 +1,27 @@
 import React from 'react'
 
-import { Card, Icon, Label } from 'semantic-ui-react'
+import { Card, Icon, Label, Image, Button } from 'semantic-ui-react'
 import moment from 'moment'
+import { Link } from 'react-router-dom'
 
 const PostCard = ( { post: { body, createdAt, id, username, likeCount, commentCount, likes} } ) => {
+    const likePost = () => {
+        console.log('like post')
+    }
+    
     return (
-        <Card>
+        <Card fluid>
             <Card.Content>
                 <Image floated='right' size='mini' src='https://react.semantic-ui.com/images/avatar/large/molly.jpg' />
                 <Card.Header>{username}</Card.Header>
-                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow(true)}</Card.Meta>
                 <Card.Description>{body}</Card.Description>
             </Card.Content>
             <Card.Content extra>
-                <p>buttons here</p>
+            <Button as='div' labelPosition='right' onClick={likePost}>
+                <Button color='teal' basic><Icon name='heart' /></Button>
+                <Label basic color='teal' pointing='left'>{likeCount}</Label>
+            </Button>
             </Card.Content>
         </Card>
     )
