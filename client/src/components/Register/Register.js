b05c30f25226c3259612e9610f7f19730f948f91
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-14 18:53:02 +0200

    Starting to work on authentication

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form } from 'semantic-ui-react'
 
 const Register = () => {
+    const [values, setValues] = useState({
+        username: '',
+        password: '',
+        email: '',
+        confirmPassword: ''
+    })
+
     return (
         <div>
-            Register
+            <Form onSubmit={onSubmit} noValidate>
+                <h1>Register</h1>
+                <Form.Input label="Username" placeholder="Username" name="username" value={values.username} onChange={onChange} />
+            </Form>
         </div>
     )
 }
