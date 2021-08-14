9eae909beb541fd3c83de1504d679f2127d804d1
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-14 19:10:56 +0200

    Working on Error handeling

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -6,6 +6,7 @@ import { Button, Form } from 'semantic-ui-react'
 import './styles.css'
 
 const Register = () => {
+    const [errors, setErrors] = useState({})
     const [values, setValues] = useState({
         username: '',
         password: '',
@@ -21,6 +22,9 @@ const Register = () => {
         update(proxy, result) {
             console.log(result)
         },
+        onError(err) {
+            setErrors(err.graphQLErrors[0].extensions.exception.errors)
+        },
         variables: values
     })
 
@@ -31,12 +35,12 @@ const Register = () => {
 
     return (
         <div className='form-container'>
-            <Form onSubmit={onSubmit} noValidate>
+            <Form onSubmit={onSubmit} noValidate className={loading && "loading"}>
                 <h1>Register</h1>
-                <Form.Input label="Username" placeholder="Username" name="username" value={values.username} onChange={onChange} />
-                <Form.Input label="Email" placeholder="Email" name="Email" value={values.email} onChange={onChange} />
-                <Form.Input label="Password" placeholder="Password" name="password" value={values.password} onChange={onChange} />
-                <Form.Input label="Confrim Password" placeholder="Confrim Password" name="confrimPassword" value={values.confirmPassword} onChange={onChange} />
+                <Form.Input label="Username" placeholder="Username" name="username" type="username" value={values.username} onChange={onChange} />
+                <Form.Input label="Email" placeholder="Email" name="Email" type="email" value={values.email} onChange={onChange} />
+                <Form.Input label="Password" placeholder="Password" name="password" type="password" value={values.password} onChange={onChange} />
+                <Form.Input label="Confrim Password" placeholder="Confrim Password" name="confrimPassword" type="password" value={values.confirmPassword} onChange={onChange} />
                 <Button type="submit" primary>Register</Button>
             </Form>
         </div>
