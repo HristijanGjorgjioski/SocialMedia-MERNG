b3fe1e593f110644ec7152003b7f5d1e074e65c3
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-20 19:02:44 +0200

    Working on PostForm component

--- client/src/components/PostForm/PostForm.js
+++ client/src/components/PostForm/PostForm.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Button, Form } from 'semantic-ui-react'
+
+import { useForm } from '../../util/hooks'
 
 const PostForm = () => {
+    const { values, onChange, onSubmit } = useForm(createPostCallback)
+
     return (
-        <div>
-            
-        </div>
+        <Form onSubmit={onSubmit}>
+            <h2>Create a post:</h2>
+            <Form.Field>
+                <Form.Input
+                    placeholder="MERNG"
+                    name="body"
+                    onChange={onChange}
+                    value={values.body}
+                />
+                <Button type="submit" color="teal">Submit</Button>
+                </Form.Field>
+        </Form>
     )
 }
 
