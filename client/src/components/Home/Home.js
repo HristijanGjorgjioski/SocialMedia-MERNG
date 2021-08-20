bc13a7674f8d88554d74faf19ee0ab1b0d55be7c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-20 18:58:51 +0200

    Working on form for adding posts

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,15 +1,17 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
-
 import { Grid } from 'semantic-ui-react'
+
 import PostCard from '../PostCard/PostCard'
+import { AuthContext } from '../../context/auth' 
 
 import './styles.css'
+import PostForm from '../PostForm/PostForm'
 
 const Home = () => {
     const { loading, data: { getPosts: posts } } = useQuery(FETCH_POSTS_QUERY)
+    const { user } = useContext(AuthContext)
 
     return (
         <Grid columns={3}>
@@ -17,6 +19,11 @@ const Home = () => {
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
+                {user && (
+                    <Grid.Column>
+                        <PostForm />
+                    </Grid.Column>
+                )}
                 {loading ? (
                     <h1>Loading posts...</h1>
                 ) : (
