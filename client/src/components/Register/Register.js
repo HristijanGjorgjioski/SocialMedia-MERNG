f6af5594afcab56e5f51755cbd20d9f4af2b2f69
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-20 11:57:49 +0200

    Importing Login function from AuthContext

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import gql from 'graphql-tag'
 import { useMutation } from '@apollo/react-hooks'
 import { Button, Form } from 'semantic-ui-react'
 
 import './styles.css'
 import { useForm } from '../../util/hooks'
+import { AuthContext } from '../../context/auth'
 
 const Register = ({ history }) => {
     const [errors, setErrors] = useState({})
+    const { login } = useContext(AuthContext)
 
     const {  onChange, onSubmit, values } = useForm(registerUser, {
         username: '',
@@ -17,7 +19,8 @@ const Register = ({ history }) => {
     })
 
     const [addUser, { loading }] = useMutation(REGISTER_USER, {
-        update(_, result) {
+        update(_, { data: { register: userData } }) {
+            login(userData)
             history.push('/')
         },
         onError(err) {
