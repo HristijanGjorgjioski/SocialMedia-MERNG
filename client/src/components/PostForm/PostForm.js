67d205d4c6549a37729e0321a18f0254c1fa59fa
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-22 15:30:32 +0200

    Error handeling is done

--- client/src/components/PostForm/PostForm.js
+++ client/src/components/PostForm/PostForm.js
@@ -43,7 +43,7 @@ const PostForm = () => {
             </Form.Field>
         </Form>
         {error && (
-            <div className="ui error message">
+            <div className="ui error message" style={{ marginBottom: 20 }}>
                 <ul className="list">
                     <li>{error.graphQLErrors[0].message}</li>
                 </ul>
