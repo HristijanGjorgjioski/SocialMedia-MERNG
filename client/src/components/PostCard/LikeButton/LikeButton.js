7385659205cd3e0954089d4eaae66fa8d069b592
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-08-25 19:16:45 +0200

    Creating Like logic

--- client/src/components/PostCard/LikeButton/LikeButton.js
+++ client/src/components/PostCard/LikeButton/LikeButton.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Label } from 'semantic-ui-react'
 
 const LikeButton = ({ post: { id, likeCount, likes } }) => {
+    const [liked, setLiked] = useState(false)
+
+    useEffect(() => {
+        if(user && likes.find(like => like.username === user.username)) {
+            return setLiked(true)
+        }
+        return setLiked(false)
+    }, [user, likes])
+
     return (
         <div>
             <Button as='div' labelPosition='right' onClick={likePost}>
